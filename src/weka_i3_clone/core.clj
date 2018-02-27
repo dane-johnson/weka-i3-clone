759f86e9f96c894471693a7b3886b05300f36558
Dane Johnson <dane_johnson@ymail.com>
2018-02-26 18:58:38 -0600

    Able to parse arff files

--- src/weka_i3_clone/core.clj
+++ src/weka_i3_clone/core.clj
@@ -1,7 +1,34 @@
+;; Dane Johnson
+;; March 14 2018
+;; This program seeks to replicate the ID3 method
 (ns weka-i3-clone.core
+  (:require [clojure.string :refer [split]])
   (:gen-class))
+
+(defrecord Attribute [id vals])
+(defrecord Arff [attributes data])
+
+(defn create-attribute
+  [line]
+  (->Attribute (second (re-seq #"\w+" line))
+               (->> (re-find #"\{(.*)\}" line)
+                    second
+                    (re-seq #"[^\s,]+"))))
+(defn create-data
+  [line]
+  (split line #","))
+
+(defn read-arff-file
+  [stream]
+  (->Arff (map create-attribute (re-seq #"@attribute.*" stream))
+          (->> (re-find #"@data\r?\n((.*\r?\n)+)" stream)
+                second
+                (re-seq #"(\S*)\r?\n")
+                (map second)
+                (map create-data))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (-> (read-arff-file (slurp (first args)))
+      (prn)))
