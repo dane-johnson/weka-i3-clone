d2f322e125fb69aef9ce0b924eb030bfc114b020
Dane Johnson <dane_johnson@ymail.com>
2018-02-26 19:21:00 -0600

    Added docstring

--- src/weka_i3_clone/core.clj
+++ src/weka_i3_clone/core.clj
@@ -5,20 +5,27 @@
   (:require [clojure.string :refer [split]])
   (:gen-class))
 
+;; An Attribute holds an id and values
 (defrecord Attribute [id vals])
+;; An Arff holds attributes and data
 (defrecord Arff [attributes data])
 
 (defn create-attribute
+  "Creates an attribute from a string"
   [line]
   (->Attribute (second (re-seq #"\w+" line))
                (->> (re-find #"\{(.*)\}" line)
                     second
-                    (re-seq #"[^\s,]+"))))
+                    (re-seq #"[^\s,]+")
+                    set)))
+
 (defn create-data
+  "Creates a data vector from a string"
   [line]
   (split line #","))
 
 (defn read-arff-file
+  "Reads a file into an Arff"
   [stream]
   (->Arff (map create-attribute (re-seq #"@attribute.*" stream))
           (->> (re-find #"@data\r?\n((.*\r?\n)+)" stream)
@@ -27,8 +34,9 @@
                 (map second)
                 (map create-data))))
 
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Reads an arff file into an arff object"
   [& args]
   (-> (read-arff-file (slurp (first args)))
       (prn)))
